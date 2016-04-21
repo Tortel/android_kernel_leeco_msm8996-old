54d9e04203e294d07b81d4470abd3e1341438a75
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2016-04-21 17:08:32 +0530

    msm: mdss: do not report overflow errors during read commands
    During a DSI DMA read command transaction, it is expected to have
    DSI FIFO overflow if the read operation duration exceeds one BLLP
    line time frame. Make sure not to trigger DSI FIFO overflow recovery
    sequence for such cases where DSI FIFO overflow error is masked.
    
    Change-Id: I0762a0f51d580a44cfa24ffac60cc465c241c733
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -2871,6 +2871,14 @@ static bool mdss_dsi_fifo_status(struct mdss_dsi_ctrl_pdata *ctrl)
 	if (status & 0xcccc4409) {
 		MIPI_OUTP(base + 0x000c, status);
 		pr_err("%s: status=%x\n", __func__, status);
+
+		/*
+		 * if DSI FIFO overflow is masked,
+		 * do not report overflow error
+		 */
+		if (MIPI_INP(base + 0x10c) & 0xf0000)
+			status = status & 0xaaaaffff;
+
 		if (status & 0x44440000) {/* DLNx_HS_FIFO_OVERFLOW */
 			dsi_send_events(ctrl, DSI_EV_DLNx_FIFO_OVERFLOW, 0);
 			/* Ignore FIFO EMPTY when overflow happens */
